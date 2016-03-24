f0acad59255fc61c05b788d123225b5084b5c69c
Sass David <david.sass14@gmail.com>
2016-03-24 01:12:13 +0100

    rotate robot

--- teszt/MainWindow.xaml.cs
+++ teszt/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ namespace teszt {
         private const int MyImageSizeX = 640;
         private const int MyImageSizeY = 640;
         private readonly ShapeDrawer sd = new ShapeDrawer();
+        private BitmapSource _myOriginalMap;
         private CsvToMatrix map;
         private BitmapSource myBitmapSource;
         private byte[] pixels;
@@ -23,10 +24,11 @@ namespace teszt {
             MessageHandler.TextBoxMessages = TextBoxForMessages;
 
 
-            if (CheckBoxLogOnOff.IsChecked.Value) TextBoxLogFileName.Visibility = Visibility.Visible;
+            if (CheckBoxLogOnOff.IsChecked != null && CheckBoxLogOnOff.IsChecked.Value) TextBoxLogFileName.Visibility = Visibility.Visible;
 
-            pixels = new byte[640 * 640 * 4];
-            myBitmapSource = BitmapSource.Create(640, 640, 96, 96, PixelFormats.Pbgra32, null, pixels, 640 * 4);
+            pixels = new byte[MyImageSizeX * MyImageSizeY * 4];
+            myBitmapSource = BitmapSource.Create(MyImageSizeX, MyImageSizeY, 96, 96, PixelFormats.Pbgra32, null, pixels,
+                                                 MyImageSizeX * 4);
             Image.Source = myBitmapSource;
             RenderOptions.SetBitmapScalingMode(Image, BitmapScalingMode.NearestNeighbor);
 
@@ -69,6 +71,8 @@ namespace teszt {
 
                 myBitmapSource = BitmapSource.Create(MyImageSizeX, MyImageSizeY, 96, 96, PixelFormats.Pbgra32, null,
                                                      pixels, MyImageSizeX * 4);
+                _myOriginalMap = BitmapSource.Create(MyImageSizeX, MyImageSizeY, 96, 96, PixelFormats.Pbgra32, null,
+                                                     pixels, MyImageSizeX * 4);
                 Image.Source = myBitmapSource;
                 RenderOptions.SetBitmapScalingMode(Image, BitmapScalingMode.NearestNeighbor);
             }
@@ -84,47 +88,64 @@ namespace teszt {
 
             Robot robot = null;
             if (RadioButton.IsChecked != null && RadioButton.IsChecked.Value)
-                robot = new Robot((int) SliderViweAngle.Value, Convert.ToInt32(TextBoxPositionX.Text),
-                                  Convert.ToInt32(TextBoxPositionY.Text),
-                                  Convert.ToInt32(TextBoxCoveringPercentage.Text), 90, "fan50A.csv");
+                if (TextBoxPositionX.Text.Length != 0 && TextBoxPositionY.Text.Length != 0 &&
+                    TextBoxCoveringPercentage.Text.Length != 0)
+                    robot = new Robot((int) SliderViweAngle.Value, Convert.ToInt32(TextBoxPositionX.Text),
+                                      Convert.ToInt32(TextBoxPositionY.Text),
+                                      Convert.ToInt32(TextBoxCoveringPercentage.Text), 90, "fan50A.csv");
+                else MessageBox.Show("Adjon meg kezdőértékeket a robotnak!");
             else if (RadioButton1.IsChecked != null && RadioButton1.IsChecked.Value)
-                robot = new Robot((int) SliderViweAngle.Value, Convert.ToInt32(TextBoxPositionX.Text),
-                                  Convert.ToInt32(TextBoxPositionY.Text),
-                                  Convert.ToInt32(TextBoxCoveringPercentage.Text), 90, "fan100.csv");
+                if (TextBoxPositionX.Text.Length != 0 && TextBoxPositionY.Text.Length != 0 &&
+                    TextBoxCoveringPercentage.Text.Length != 0)
+                    robot = new Robot((int) SliderViweAngle.Value, Convert.ToInt32(TextBoxPositionX.Text),
+                                      Convert.ToInt32(TextBoxPositionY.Text),
+                                      Convert.ToInt32(TextBoxCoveringPercentage.Text), 90, "fan100.csv");
+                else MessageBox.Show("Adjon meg kezdőértékeket a robotnak!");
 
             if (robot != null) {
                 var stride = myBitmapSource.PixelWidth * 4;
                 var size = myBitmapSource.PixelHeight * stride;
                 pixels = new byte[size];
+                _myOriginalMap.CopyPixels(pixels, stride, 0);
 
-                var result = new bool[640, 640];
+                robot.Rotate(90);
 
-                for (var i = 0; i < 640; i++) for (var j = 0; j < 640; j++) result[i, j] = map.Map[i, j];
-
-
-                for (var i = robot.X; i < robot.X + robot.Robot1.Map.GetLength(0); i++)
-                    for (var j = robot.Y; j < robot.Y + robot.Robot1.Map.GetLength(1); j++)
-                        if (map.Map[i, j] && robot.Robot1.Map[i - robot.X, j - robot.Y]) result[i, j] = true;
-                        else if (!map.Map[i, j] && robot.Robot1.Map[i - robot.X, j - robot.Y]) result[i, j] = true;
-                        else if (!map.Map[i, j] && !robot.Robot1.Map[i - robot.X, j - robot.Y]) result[i, j] = false;
-                        else if (map.Map[i, j] && !robot.Robot1.Map[i - robot.X, j - robot.Y]) result[i, j] = false;
+                var stride1 = robot.MyBitmap.PixelWidth * 4;
+                var size1 = robot.MyBitmap.PixelHeight * stride;
+                var robotPix = new byte[size1];
+                robot.MyBitmap.CopyPixels(robotPix, stride1, 0);
 
 
-                var current = 0;
-                for (var i = 0; i < result.GetLength(0); i++)
-                    for (var j = 0; j < result.GetLength(1); j++)
-                        if (result[i, j]) {
-                            pixels[current++] = 255;
-                            pixels[current++] = 255;
-                            pixels[current++] = 255;
-                            pixels[current++] = 255;
+                for (var i = 0; i < robot.Robot1.Map.GetLength(0) * 4; i += 4)
+                    for (var j = 0; j < robot.Robot1.Map.GetLength(1); j += 1)
+                        if (pixels[i + j * 4 * 640] == 255 && robotPix[i + j * 4 * robot.Robot1.Map.GetLength(1)] == 255) {
+                            pixels[i + j * 4 * 640] = 255;
+                            pixels[i + j * 4 * 640 + 1] = 255;
+                            pixels[i + j * 4 * 640 + 2] = 255;
+                            pixels[i + j * 4 * 640 + 3] = 255;
                         }
-                        else {
-                            pixels[current++] = 0;
-                            pixels[current++] = 0;
-                            pixels[current++] = 0;
-                            pixels[current++] = 255;
+                        else if (pixels[i + j * 4 * 640] == 0 &&
+                                 robotPix[i + j * 4 * robot.Robot1.Map.GetLength(1)] == 255) {
+                            pixels[i + j * 4 * 640] = 255;
+                            pixels[i + j * 4 * 640 + 1] = 255;
+                            pixels[i + j * 4 * 640 + 2] = 255;
+                            pixels[i + j * 4 * 640 + 3] = 255;
                         }
+                        else if (pixels[i + j * 4 * 640] == 0 &&
+                                 robotPix[i + j * 4 * robot.Robot1.Map.GetLength(1)] == 0) {
+                            pixels[i + j * 4 * 640] = 0;
+                            pixels[i + j * 4 * 640 + 1] = 0;
+                            pixels[i + j * 4 * 640 + 2] = 0;
+                            pixels[i + j * 4 * 640 + 3] = 255;
+                        }
+                        else if (pixels[i + j * 4 * 640] == 255 &&
+                                 robotPix[i + j * 4 * robot.Robot1.Map.GetLength(1)] == 0) {
+                            pixels[i + j * 4 * 640] = 0;
+                            pixels[i + j * 4 * 640 + 1] = 0;
+                            pixels[i + j * 4 * 640 + 2] = 0;
+                            pixels[i + j * 4 * 640 + 3] = 255;
+                        }
+
 
                 myBitmapSource = BitmapSource.Create(MyImageSizeX, MyImageSizeY, 96, 96, PixelFormats.Pbgra32, null,
                                                      pixels, MyImageSizeX * 4);
--- teszt/Robot.cs
+++ teszt/Robot.cs
@@ -1,15 +1,23 @@
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
 namespace teszt {
     internal class Robot {
-
         public Robot(int angleview, int x, int y, int cover, double theta, string robotName) {
-            Robot1= new CsvToMatrix(robotName);
+            Robot1 = new CsvToMatrix(robotName);
             Robot1.Read();
+            BoolMatrixToBitmap();
             AngleView = angleview;
             X = x;
             Y = y;
             Cover = cover;
             Theta = theta;
         }
+
+        public BitmapSource MyBitmap { get; private set; }
+
         public CsvToMatrix Robot1 { get; }
         public double Theta { get; set; }
 
@@ -20,5 +28,113 @@ namespace teszt {
         public int X { get; set; }
 
         public int AngleView { get; set; }
+
+        private void BoolMatrixToBitmap() {
+            var pixels = new byte[Robot1.Map.GetLength(0) * Robot1.Map.GetLength(1) * 4];
+            var current = 0;
+            for (var i = 0; i < Robot1.Map.GetLength(0); i++)
+                for (var j = 0; j < Robot1.Map.GetLength(1); j++)
+                    if (Robot1.Map[i, j]) {
+                        pixels[current++] = 255;
+                        pixels[current++] = 255;
+                        pixels[current++] = 255;
+                        pixels[current++] = 255;
+                    }
+                    else {
+                        pixels[current++] = 0;
+                        pixels[current++] = 0;
+                        pixels[current++] = 0;
+                        pixels[current++] = 255;
+                    }
+            MyBitmap = BitmapSource.Create(Robot1.Map.GetLength(0), Robot1.Map.GetLength(1), 96, 96,
+                                           PixelFormats.Pbgra32, null, pixels, Robot1.Map.GetLength(0) * 4);
+        }
+
+        public void Rotate(double rotAngle) {
+            var stride = MyBitmap.PixelWidth * 4;
+            var size = MyBitmap.PixelHeight * stride;
+            var pixArray = new byte[size];
+            var coordX = new float[0];
+            var coordY = new float[0];
+            MyBitmap.CopyPixels(pixArray, stride, 0);
+            for (var i = 0; i < Robot1.Map.GetLength(0) * 4; i += 4)
+                for (var j = 0; j < Robot1.Map.GetLength(1); j += 1)
+                    if (pixArray[i + j * 4 * Robot1.Map.GetLength(1)] == 255) {
+                        Array.Resize(ref coordX, coordX.Length + 1);
+                        Array.Resize(ref coordY, coordY.Length + 1);
+
+                        coordX[coordX.Length - 1] = i / 4 - MyBitmap.PixelWidth / 2;
+                        coordY[coordY.Length - 1] = j - MyBitmap.PixelHeight / 2;
+                    }
+
+            var originalCoordinates = new float[coordX.Length, 2];
+            for (var i = 0; i < coordX.Length; i++) {
+                originalCoordinates[i, 0] = coordX[i];
+                originalCoordinates[i, 1] = coordY[i];
+            }
+            float[,] rotMatrix = {
+                                     {(float) Math.Cos(rotAngle), (float) -Math.Sin(rotAngle)},
+                                     {(float) Math.Sin(rotAngle), (float) Math.Cos(rotAngle)}
+                                 };
+
+            var resultMatrix = MatrixMult(originalCoordinates, rotMatrix);
+
+
+            for (var i = 0; i < Robot1.Map.GetLength(0) * 4; i += 4)
+                for (var j = 0; j < Robot1.Map.GetLength(1); j = j + 1) {
+                    pixArray[i + j * 4 * Robot1.Map.GetLength(1)] = 0; //r
+                    pixArray[i + j * 4 * Robot1.Map.GetLength(1) + 1] = 0; // g 
+                    pixArray[i + j * 4 * Robot1.Map.GetLength(1) + 2] = 0; // b 
+                    pixArray[i + j * 4 * Robot1.Map.GetLength(1) + 3] = 255; // alpha 
+                }
+
+
+            for (var i = 0; i < resultMatrix.GetLength(0); i++) {
+                var pixelUp = (int) Math.Ceiling(resultMatrix[i, 0] + MyBitmap.PixelHeight / 2) * 4 +
+                              (int) Math.Ceiling(resultMatrix[i, 1] + MyBitmap.PixelWidth / 2) * 4 *
+                              Robot1.Map.GetLength(1);
+                var pixelDown = (int) Math.Floor(resultMatrix[i, 0] + MyBitmap.PixelHeight / 2) * 4 +
+                                (int) Math.Floor(resultMatrix[i, 1] + MyBitmap.PixelWidth / 2) * 4 *
+                                Robot1.Map.GetLength(1);
+
+                try // TODO: remove try-catch
+                {
+                    if (pixelUp > 0 &&
+                        pixelUp <= MyBitmap.PixelWidth * 4 + MyBitmap.PixelHeight * 4 * Robot1.Map.GetLength(1)) {
+                        pixArray[pixelUp] = 255; // r
+                        pixArray[pixelUp + 1] = 255; // g 
+                        pixArray[pixelUp + 2] = 255; // b 
+                        pixArray[pixelUp + 3] = 255; // alpha 
+                    }
+                    if (pixelDown > 0 &&
+                        pixelDown <= MyBitmap.PixelWidth * 4 + MyBitmap.PixelHeight * 4 * Robot1.Map.GetLength(1)) {
+                        pixArray[pixelDown] = 255; // r
+                        pixArray[pixelDown + 1] = 255; // g 
+                        pixArray[pixelDown + 2] = 255; // b 
+                        pixArray[pixelDown + 3] = 255; // alpha 
+                    }
+                }
+                catch {
+                    // ignored
+                }
+            }
+            MyBitmap = BitmapSource.Create(Robot1.Map.GetLength(0), Robot1.Map.GetLength(1), 96, 96,
+                                           PixelFormats.Pbgra32, null, pixArray, Robot1.Map.GetLength(0) * 4);
+        }
+
+        private float[,] MatrixMult(float[,] a, float[,] b) {
+            float[,] c = {};
+            if (a.GetLength(1) == b.GetLength(0)) {
+                c = new float[a.GetLength(0), b.GetLength(1)];
+                for (var i = 0; i < c.GetLength(0); i++)
+                    for (var j = 0; j < c.GetLength(1); j++) {
+                        c[i, j] = 0;
+                        for (var k = 0; k < a.GetLength(1); k++) // vagy k<b.GetLength(0)
+                            c[i, j] = c[i, j] + a[i, k] * b[k, j];
+                    }
+            }
+            else MessageBox.Show("Matrix size mismatch.");
+            return c;
+        }
     }
 }
