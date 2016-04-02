9117685117c18d3bd6390f15905916fa6270bc9f
Sass David <david.sass14@gmail.com>
2016-04-02 15:27:18 +0200

    error handling

--- teszt/Dialog.xaml.cs
+++ teszt/Dialog.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System.Windows;
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace teszt {
     public partial class Dialog {
         public Dialog() {
             InitializeComponent();
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            Button.IsEnabled = false;
         }
 
         public string ResponseText => TextBox.Text;
@@ -13,6 +17,16 @@ namespace teszt {
 
         private void button_Click(object sender, RoutedEventArgs e) {
             DialogResult = true;
+        }
+
+        private void IntegerValidationTextBox(object sender, TextCompositionEventArgs e) {
+            var regex = new Regex(@"^\d$");
+            e.Handled = !regex.IsMatch(e.Text);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            if (TextBox1.Visibility == Visibility.Hidden) Button.IsEnabled = TextBox.Text.Length > 0;
+            else Button.IsEnabled = TextBox.Text.Length > 0 && TextBox1.Text.Length > 0;
         }
     }
 }
