96f92ccc5a0db72b83d4f4d7e8a65487ed514012
Tamas.Kepes <kepestomi04@gmail.com>
2016-04-17 11:21:46 +0200

    Add RobotHandler

---
+++ teszt/RobotHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt
+{
+    public class RobotHandler
+    {
+        List<Tuple<int, int, double>> coordinates;
+        //TODO add common variable
+        public List<Tuple<int, int, double>> getCurrentCoordinates() {
+            return coordinates;
+        }
+        public void setCurrentCoordinates(List<Tuple<int, int, double>> coords)
+        {
+            coordinates=coords;
+        }
+        //A jelenlegi pozicioját adja vissza ezen a metóduson keresztül az algoritmusos csoportok
+
+        //void setCurrentCoordinates(); //Itt beallíthatják
+
+    }
+}
