02708bb03d8244f398d43a96a85c105d8be0139b
Tamas.Kepes <kepestomi04@gmail.com>
2016-04-17 12:36:35 +0200

    Improve RobotHandler

--- teszt/RobotHandler.cs
+++ teszt/RobotHandler.cs
@@ -7,18 +7,34 @@ using System.Threading.Tasks;
 namespace teszt
 {
     //ezt az osztalyt, amiből objektum lesz, ez tartalmazza majd a robot jelenlegi poziciojat
+    //A jelenleg lefedett területet, ezekkel tudnak számolni az algosok.
     //az ebből az osztályból képzett objektum oljda meg a kommunikációt a az algoritmusok és a GUI között
 
     public class RobotHandler
     {
-        List<Tuple<int, int, double>> coordinates;
-        //TODO add common variable
-        public List<Tuple<int, int, double>> getCurrentCoordinates() {
-            return coordinates;
-        }
-        public void setCurrentCoordinates(List<Tuple<int, int, double>> coords)
+        List<Tuple<int, int, double>> Coordinates;
+        List<Tuple<int, int>> CurrentlyCoveredArea;
+
+        public RobotHandler()
         {
-            coordinates=coords;
+       
+        }
+        public RobotHandler(List<Tuple<int, int, double>> coords, List<Tuple<int, int>> coveredarea)
+        {
+            Coordinates = coords;
+            CurrentlyCoveredArea = coveredarea;
+        }
+        public List<Tuple<int, int, double>> getCurrentCoordinates() {
+            return Coordinates;
+        }
+        public void setCurrentlyCoveredArea(List<Tuple<int, int>> coveredarea)
+        {
+            CurrentlyCoveredArea = coveredarea;
+        }
+     
+        public List<Tuple<int, int>> getCurrentCoveredArea()
+        {
+            return CurrentlyCoveredArea;
         }
 
 
