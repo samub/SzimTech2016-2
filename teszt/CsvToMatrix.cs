48fd8303b534dbd9ffd38e5d9230ae838ca86707
Tamas.Kepes <kepestomi04@gmail.com>
2016-04-17 09:55:39 +0200

    Add Comments to some methods

--- teszt/CsvToMatrix.cs
+++ teszt/CsvToMatrix.cs
@@ -24,7 +24,7 @@ namespace teszt {
             while (_sr.ReadLine() != null) lineNum ++;
             return lineNum;
         }
-
+        //Read the csv from file
         public void Read() {
             var lineCount = LineCount();
             _sr.DiscardBufferedData();
