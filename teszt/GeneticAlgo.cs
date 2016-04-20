b21aacb3b7e73a2c5ed852d5e5b344054352b34d
samub <balintsamu@gmail.com>
2016-04-20 20:30:03 +0200

    Genetic algo Start()

---
+++ teszt/GeneticAlgo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotMover
+{
+    class GeneticAlgo
+    {
+        // Itt kezdődik a GA algó
+        public static void Start()
+        {
+
+        }
+    }
+}
--- teszt/MainWindow.xaml.cs
+++ teszt/MainWindow.xaml.cs
@@ -166,7 +166,7 @@ namespace RobotMover {
             if (_robot != null) {
                 MapRefresh();
 
-                if (RadioButtonGenetic.IsChecked != null && RadioButtonGenetic.IsChecked.Value) MessageBox.Show("Generikus");
+                if (RadioButtonGenetic.IsChecked != null && RadioButtonGenetic.IsChecked.Value) GeneticAlgo.Start();
                 else if (RadioButtonHeuristic1.IsChecked != null && RadioButtonHeuristic1.IsChecked.Value) {
                     _mapToBool = BitmapToBools(_myOriginalMap);
 
